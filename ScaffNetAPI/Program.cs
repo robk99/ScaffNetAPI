9214279d5f461b451d9af158c1bc22dc18d2ef27
DESKTOP-NDCBFHM\MrUser <robert.kamenski.rld@gmail.com>
2025-03-08 22:46:27 +0100

    feat: Add ScaffEventHandler with simple logger

--- ScaffNetAPI/Program.cs
+++ ScaffNetAPI/Program.cs
@@ -1,42 +1,66 @@
-var builder = WebApplication.CreateBuilder(args);
+using ScaffNet.Utils;
+using ScaffNetAPI.Utils;
 
-// Add services to the container.
-
-var services = builder.Services;
-
-services.AddControllers();
-
-services.AddHealthChecks();
-services.AddSwaggerGen();
-
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
-services.AddCors(options =>
+internal class Program
 {
-    options.AddPolicy("CorsPolicy", policy =>
-        policy.WithOrigins(allowedOrigins)
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials());
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
 
-// Configure the HTTP request pipeline.
+        var services = builder.Services;
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        services.AddControllers();
+
+        services.AddHealthChecks();
+        services.AddSwaggerGen();
+
+        services.AddScoped<ScaffEventHandler>();
+
+        var config = builder.Configuration;
+        var allowedOrigins = config.GetSection("AllowedOrigins").Get<string[]>();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy =>
+                policy.WithOrigins(allowedOrigins)
+                      .AllowAnyMethod()
+                      .AllowAnyHeader()
+                      .AllowCredentials());
+        });
+
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+            .AddConfiguration(config.GetSection("Logging"))
+            .AddConsole();
+        });
+
+        var logger = loggerFactory.CreateLogger<Program>();
+
+        var eventHandler = services.BuildServiceProvider().GetService<ScaffEventHandler>();
+        ScaffUtils.SetEventHandler(eventHandler);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHealthChecks("/health");
+
+        app.UseHttpsRedirection();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHealthChecks("/health");
-
-app.UseHttpsRedirection();
-
-app.UseCors("CorsPolicy");
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ ScaffNetAPI/Utils/ScaffEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using ScaffNet.Utils.EventHandling;
+
+namespace ScaffNetAPI.Utils
+{
+    internal class ScaffEventHandler : IScaffEventHandler
+    {
+        private ILogger<ScaffEventHandler> _logger { get; set; }
+        public ScaffEventHandler(ILogger<ScaffEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnDebug(string message)
+        {
+            _logger.LogDebug(message);
+        }
+
+        public void OnInfo(string message)
+        {
+            _logger.LogInformation(message);
+        }
+
+        public void OnWarning(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+        public void OnError(string message)
+        {
+            _logger.LogError(message);
+        }
+
+        public void OnCritical(string message)
+        {
+            _logger.LogCritical(message);
+        }
+    }
+}
